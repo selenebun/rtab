9255160e01307c32796c3a92cfda82912067d643
Amelia Clarke <amelia.amok@gmail.com>
2021-04-24 07:33:25 -0500

    Add basic table formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::Write;
 use std::process;
 
 use clap::{crate_authors, crate_version, App, Arg};
@@ -9,6 +11,13 @@ fn main() {
         .author(crate_authors!())
         .about("Generate tables from CSV.")
         .arg(Arg::with_name("FILE").required(true))
+        .arg(
+            Arg::with_name("STYLE")
+                .long("style")
+                .help("Sets table style")
+                .takes_value(true)
+                .possible_values(&["basic"]),
+        )
         .get_matches();
 
     // Open input file for reading.
@@ -20,6 +29,47 @@ fn main() {
             process::exit(1);
         }
     };
+
+    // Generate formatted table.
+    let style = matches.value_of("STYLE").unwrap_or("basic");
+    let output = match style {
+        "basic" => basic_table(&records),
+        _ => unreachable!(),
+    };
+
+    // Print table.
+    match output {
+        Ok(output) => print!("{}", output),
+        Err(e) => {
+            eprintln!("Error formatting output: {}", e);
+            process::exit(1);
+        }
+    }
+}
+
+/// Generate a basic table.
+fn basic_table(records: &[StringRecord]) -> Result<String, Box<dyn Error>> {
+    // Calculate the length of each record.
+    let len = records.first().map_or(0, |r| r.len());
+
+    // Find the maximum width in each column.
+    let widths = records.iter().fold(vec![0; len], |acc, r| {
+        acc.iter()
+            .zip(r.iter())
+            .map(|e| (*e.0).max(e.1.len()))
+            .collect()
+    });
+
+    // Build output string.
+    let mut output = String::new();
+    for record in records {
+        for (i, field) in record.iter().enumerate() {
+            write!(&mut output, "{:width$}", field, width = widths[i] + 1)?;
+        }
+        writeln!(&mut output)?;
+    }
+
+    Ok(output)
 }
 
 /// Read records from file.
